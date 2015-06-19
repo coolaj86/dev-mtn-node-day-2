9091125d8cd17a76ee3e451f1c5401d1b531d746
AJ ONeal <awesome@coolaj86.com>
2015-06-19 11:54:05 -0600

    use https

--- serve.js
+++ serve.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var http = require('http');
+var https = require('https');
+
 var server = http.createServer();
+var secureServer = https.createServer(require('localhost.daplie.com-certificates'));
+
 var port = process.argv[2] || 3000;
 var app = require('./app');
 
@@ -9,4 +13,10 @@ server.on('request', app());
 
 server.listen(port, function () {
   console.log("I'm ready!");
+});
+
+secureServer.on('request', app());
+// 443
+secureServer.listen('8443', function () {
+  console.log('secure and ready');
 });
